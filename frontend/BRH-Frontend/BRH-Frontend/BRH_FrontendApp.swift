75ec415daccd0f9249bfb4e64b207e6cdaf99f0a
Ronald Jabouin <rj336@cornell.edu>
2024-10-04 21:28:12 -0400

    xcode project

---
+++ frontend/BRH-Frontend/BRH-Frontend/BRH_FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  BRH_FrontendApp.swift
+//  BRH-Frontend
+//
+//  Created by Ronald Jabouin on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BRH_FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/BRH-Frontend/BRH-Frontend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BRH-Frontend
+//
+//  Created by Ronald Jabouin on 10/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
