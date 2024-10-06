af036e6c0f168b9a24d1fd8537761b75a46d5a0b
Ronald Jabouin <rj336@cornell.edu>
2024-10-06 00:07:19 -0400

    add splash screen before onboarding view

--- frontend/BRH-Frontend/BRH-Frontend/BRH_FrontendApp.swift
+++ frontend/BRH-Frontend/BRH-Frontend/BRH_FrontendApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BRH_FrontendApp: App {
     var body: some Scene {
         WindowGroup {
-            MacroOnboardingView()
+            SplashScreenView()
         }
     }
 }
